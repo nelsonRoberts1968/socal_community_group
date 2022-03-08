af82daf3634ed0f53b6e6dd81a4d8f216e307edd
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-03-08 14:39:33 -0800

    initial layout-components

--- so_cal_community/src/App.js
+++ so_cal_community/src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import Calendar from "./components/Calendar";
+import Culture from "./components/Culture-History";
+import Membership from "./components/Membership";
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [aboutSelected, setAboutSelected] = useState(true);
+  const [calendarSelected, setCalendarSelected] = useState(false);
+  const [cultureSelected, setCultureSelected] = useState(false);
+  const [gallerySelected, setGallerySelected] = useState(false);
+  const [membershipSelected, setMembershipSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Nav
+          contactSelected={contactSelected}
+          setContactSelected={setContactSelected}
+          aboutSelected={aboutSelected}
+          setAboutSelected={setAboutSelected}
+          calendarSelected={calendarSelected}
+          setCalendarSelected={setCalendarSelected}
+          cultureSelected={cultureSelected}
+          setCultureSelected={setCultureSelected}
+          gallerySelected={gallerySelected}
+          setGallerySelected={setGallerySelected}
+          membershipSelected={membershipSelected}
+          setMembershipSelected={setMembershipSelected}
+        ></Nav>
+      </Container>
+
+      <main>
+        <Container>
+          {contactSelected ? (
+            <>
+              <Contact></Contact>
+            </>
+          ) : (
+            <></>
+          )}
+          {aboutSelected ? (
+            <>
+              <About></About>
+            </>
+          ) : (
+            <></>
+          )}
+          {calendarSelected ? (
+            <>
+              <Calendar></Calendar>
+            </>
+          ) : (
+            <></>
+          )}
+          {cultureSelected ? (
+            <>
+              <Culture></Culture>
+            </>
+          ) : (
+            <></>
+          )}
+          {gallerySelected ? (
+            <>
+              <Gallery></Gallery>
+            </>
+          ) : (
+            <></>
+          )}
+          {membershipSelected ? (
+            <>
+              <Membership></Membership>
+            </>
+          ) : (
+            <></>
+          )}
+        </Container>
+      </main>
+      <Container>
+        <Footer></Footer>
+      </Container>
     </div>
   );
 }
