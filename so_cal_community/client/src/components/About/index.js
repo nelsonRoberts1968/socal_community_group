a3f05088578c958a33a9abb4ae9a2c514afc32d5
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-03-08 16:59:20 -0800

    setup branch

---
+++ so_cal_community/client/src/components/About/index.js
@@ -0,0 +1,165 @@
+import React from "react";
+//import coverImage from "../../assets/cover/cover-image.jpg";
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who we are</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          The SoCal Tanzanian Community is a group of people from Tanzania
+          living in Southern California consciously collaborating on the basis
+          of their belonging to the same country of origin. Collaboration could
+          be through meetings and social events. SoCal Tanzanian community
+          (Entity Number: C4564114) is a registered nonprofit membership
+          organization, based in Southern California. Members are the heartbeat
+          of the SoCal Tanzanian Community. The agreement of the Southern
+          California Tanzanian Community entered by and between the SoCal and
+          its members effective as of February 07-2020. Whereas the members
+          formed the SoCal as a nonprofit Mutual benefit corporation under the
+          laws of California.
+        </p>
+      </div>
+      <h1 id="about">Vision</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          The vision of the SoCal Tanzanian Community shall be: Striving to
+          bring together Tanzanians, and friends of Tanzania, in southern
+          California and beyond to actualize their full potential, activate
+          their entrepreneurial spirit, and strategically mobilize financial and
+          intellectual resources to ensure a brighter future for the community.
+        </p>
+      </div>
+      <h1 id="about">Mission</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          The Mission of the SoCal Tanzanian Community shall be to envision the
+          SoCal Tanzanian community as a go-to resource, strategic friendship,
+          and thought leader guiding and organizing social entrepreneurs,
+          innovators, investors, and leaders in their ambitions to create
+          positive change for the community.
+        </p>
+      </div>
+      <h1 id="about">Action Plan</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          There shall be three cardinal agendas which are Production,
+          Empowerment, and Development. These three cardinal agendas shall be
+          actualized by a team of SoCal Tanzanians whose responsibility shall be
+          to re-engineer the community to satisfy the critical needs of its
+          members and lay a solid foundation for future policies.
+        </p>
+      </div>
+      <h1 id="about">Production</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          The SoCal Tanzanian Community is a non-profit organization but will be
+          managed in a manner where all members will have some form of succor
+          for their investments (time and finance). The effort to create
+          moderate revenue for recurrent expenditures through membership fees,
+          donations, and contributions.
+        </p>
+      </div>
+      <h1 id="about">Empowerment</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          Human capacity building shall be the cornerstone of the community with
+          the recycling of goods and materials from members and external sources
+          to the needy, funerals, hospitals, etc…, when necessary. Members will
+          be encouraged to be involved.
+        </p>
+      </div>
+      <h1 id="about">Development</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>
+          The SoCal Tanzanian Community shall be well repositioned to take
+          advantage of the latest social media networking tools to reach a wider
+          audience to effectively bring change in relationship to the community
+          to better humanity.
+        </p>
+      </div>
+      <h1 id="about">Community objectives</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>The main objectives of the SoCal Tanzanian community shall be:</p>
+        <ul>
+          <li>1. To create businesses and spur entrepreneurship</li>
+          <li>2. To promote and enhance Tanzanians in southern California.</li>
+          <li>
+            3. Engage in economic, social, and cultural projects and activities
+            that are of mutual benefit.
+          </li>
+          <li>4. To connect and unites Tanzanians in southern California.</li>
+          <li>5. Helping others grieve.</li>
+          <li>6. Support each other through good times and bad</li>
+        </ul>
+      </div>
+      <h1 id="about">The Powers</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div className="my-2">
+        <p>To achieve its objectives the SoCal Tanzanian community may:</p>
+        <ul>
+          <li>1. Raise money</li>
+          <li>2. Open bank accounts</li>
+          <li>3. Take out insurance</li>
+          <li>4. Organize courses and events</li>
+          <li>5. Work with other communities and exchange information</li>
+          <li>6. Do anything lawful which will help it to fulfill its aims</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ so_cal_community/server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Event = require('./Event');
+const Membership = require('./Membership');
+
+module.exports = { User, Event, Membership };
