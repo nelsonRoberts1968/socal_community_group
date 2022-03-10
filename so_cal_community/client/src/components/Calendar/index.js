b315445f6d3222c61c40d62bfa2d6abed44efabf
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-03-09 20:29:17 -0800

    added react calendar -  does not yet store or display events

--- so_cal_community/client/src/components/Calendar/index.js
+++ so_cal_community/client/src/components/Calendar/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
-const Calendar = () => {
-    return(
-        <h1>Calendar</h1>
-    )
+function MyCalendar() {
+    const [currentEvent, setCurrentEvent] = useState(new Date());
+    return (
+        <div>
+            <Calendar onChange={setCurrentEvent} value={currentEvent} />
+        </div>
+    );
 }
 
 export default Calendar;
